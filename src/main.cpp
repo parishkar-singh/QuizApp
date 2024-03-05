da38c6cae872abdccc0d3e51893eb7ff0317a408
Parishkar Singh <parishkar.singh@esko.com>
2024-03-05 12:05:50 +0530

    fix: Few changes in the api

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,15 @@
 
 int main(int argc, char *argv[])
 {
+//  Only a console application right now
+
+// Will move to the QApplication later on
     QCoreApplication a(argc, argv);
 //    QMainWindow mainWindow;
 //    mainWindow.resize(600, 400);
 
-    APIS::Server quizApi;
-    quizApi.start();
+//    APIS::Server quizApi;
+//    quizApi.start();
 
 //    QPushButton *button = new QPushButton("Hello world! from parishkar", &mainWindow);
 //    button->move(200, 150);
